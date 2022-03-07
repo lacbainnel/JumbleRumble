67c6c06acbbd16f367385d9525eb619fce25f90d
lacbainnel <j.lacbain@yahoo.com>
2022-03-07 15:42:03 +0800

    030722-2

--- JumbleRumble.java
+++ JumbleRumble.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class JumbleRumble {
     Integer score = 0;
+    Integer guess = 3;
     private static final ArrayList<String> score1_easy1 = new ArrayList<String> (Arrays.asList("ear", "pat", "won", "top", "cat"));
     private static final ArrayList<String> score2_easy1 = new ArrayList<String> (Arrays.asList("era", "apt", "own", "opt", "act"));
     //private static final String[] score1_easy1 = new String[] {"ear", "pat", "won", "top", "cat"};
@@ -24,34 +25,43 @@ public class JumbleRumble {
             builder.append(s);
         }
         String str = builder.toString();
-        System.out.println(str);
+        //System.out.println(str);
 
         Random r = new Random();
         String word = scramble(r, str);
         System.out.println(word);
 
-        System.out.print("Enter a word: ");
-        String word1 = sc.nextLine();
-        for (int i=0; i<5; i++) {
-            if (score1_easy1.contains(word1)) {
-                System.out.println("You got it! You gained 1 point!");
-                score += 1;
-                break;
-            } else {
-                if (score2_easy1.contains(word1)) {
-                    System.out.println("You got it! You gained 2 points!");
-                    score += 2;
+        while (guess != 0) {
+            System.out.print("Enter a word: ");
+            String word1 = sc.nextLine();
+            for (int i=0; i<5; i++) {
+                if (score1_easy1.contains(word1)) {
+                    System.out.println("You got it! You gained 1 point!");
+                    score += 1;
                     break;
-                } 
-                continue;
+                } else {
+                    if (score2_easy1.contains(word1)) {
+                        System.out.println("You got it! You gained 2 points!");
+                        score += 2;
+                        break;
+                    }
+                    // }  else {
+                    //     System.out.println("Wrong! You only have " + guess + " guesses left!");
+                    //     guess--;
+                    // }
+                    // System.out.println("Wrong! You only have " + guess + " guesses left!");
+                    // guess--;
+                    continue;
+                }
+                // } else if (word1 == score2_easy1[i]){
+                //     System.out.println("You got it! You gained 2 points!");
+                //     score += 2;
+                // }
+                //continue;
             }
-            // } else if (word1 == score2_easy1[i]){
-            //     System.out.println("You got it! You gained 2 points!");
-            //     score += 2;
-            // }
-            //continue;
+            guess--;
         }
-        System.out.println(score);
+        System.out.println("Your score: " + score);
     }
 
     public static String scramble(Random random, String inputString ) {
