765004a240a480fffea3aad764317724f90972b9
lacbainnel <j.lacbain@yahoo.com>
2022-03-10 00:00:13 +0800

    031022-6

--- JumbleRumble.java
+++ JumbleRumble.java
@@ -1,5 +1,15 @@
 //package com.mycompany.jumblerumble;
 
+// import java.io.File;
+// import java.io.InputStream;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.Random;
+// import java.util.Scanner;
+// import javax.sound.sampled.Clip;
+// import javax.swing.JButton;
+// import javax.swing.JFrame;
+// import javax.sound.sampled.AudioSystem;
 import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -7,12 +17,22 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+//import javax.print.DocFlavor.URL;
 import javax.sound.sampled.AudioSystem;
+import  java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
 
 
 public class JumbleRumble {
     Integer score = 0;
     Integer guess = 5;
+    private static final ArrayList<String> baseletters_easy1 = new ArrayList<String> (Arrays.asList("e", "a", "r", "p", "t", "n", "o", "w"));  
+    private static final ArrayList<String> baseletters_easy2 = new ArrayList<String> (Arrays.asList("a", "c", "n", "e", "b", "t", "i", "s", "u")); 
     private static final ArrayList<String> easy1_store = new ArrayList<String> 
         (Arrays.asList("ear", "pat", "tap", "now", "won", "pot", "top", "cat", "era", "apt", 
                          "own", "opt", "act", "row", "tow", "tap", "war",  "oat", "are", "ate" ,
@@ -36,6 +56,10 @@ public class JumbleRumble {
     private static final ArrayList<String> easy2_words = new ArrayList<String> 
         (Arrays.asList("acne", "bane", "bent"));      
     private static ArrayList<String> easy2_guess = new ArrayList<String> ();    
+    static float previousVolume = 0;
+    static float currentVolume = 0;
+    static FloatControl fc;
+    static Boolean mute = false;
 
     public static void main (String[] args) {
         File bg = new File("bg.wav");
@@ -48,7 +72,7 @@ public class JumbleRumble {
         //String test = Arrays.toString(score1_easy1);
         // String test = TextUtils.join("",score1_easy1);
         StringBuilder builder = new StringBuilder();
-        for(String s : easy1_store) {
+        for(String s : baseletters_easy1) {
             builder.append(s);
         }
         String str = builder.toString();
@@ -142,7 +166,7 @@ public class JumbleRumble {
         //String test = Arrays.toString(score1_easy1);
         // String test = TextUtils.join("",score1_easy1);
         StringBuilder builder = new StringBuilder();
-        for(String s : easy2_store) {
+        for(String s : baseletters_easy2) {
             builder.append(s);
         }
         String str = builder.toString();
@@ -179,6 +203,7 @@ public class JumbleRumble {
             } else {
                 System.out.println("You have already guessed all of the words! Congratulations!");
                 System.out.println("Score: " + score);
+                System.exit(0);
                 //jr.easyLevelThree();
             }       
         }       
@@ -213,15 +238,72 @@ public class JumbleRumble {
     // }
 
     public static void playMusic (File Sound) {
+        JFrame f = new JFrame();
+        GridLayout gl = new GridLayout(1,3);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(gl);
+
+        JButton up = new JButton("Volume Up");
+        up.addActionListener (new ActionListener() {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                currentVolume += 1.0f;
+
+                if (currentVolume > 6.0) {
+                    currentVolume = 6.0f;
+                }
+                fc.setValue(currentVolume);
+            }
+        });
+        f.add(up);
+
+        JButton down = new JButton("Volume Down");
+        down.addActionListener (new ActionListener() {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                currentVolume -= 1.0f;
+
+                if (currentVolume < -80.0f) {
+                    currentVolume = -80.0f;
+                }
+        
+                fc.setValue(currentVolume);
+            }
+        });        
+        f.add(down);
+
+        JButton volumeMute = new JButton("Mute");
+        volumeMute.addActionListener (new ActionListener() {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                if (mute == false) {
+                    previousVolume = currentVolume;
+                    currentVolume = -80.0f;
+                    fc.setValue(currentVolume);
+                    mute = true;
+                } else if (mute == true) {
+                    currentVolume = previousVolume;
+                    fc.setValue(currentVolume);
+                    mute = false;
+                }
+            }
+        });        
+        f.add(volumeMute); 
+        
+        f.pack();
+        f.setVisible(true);
+
+
         try {
             Clip clip = AudioSystem.getClip();
             clip.open (AudioSystem.getAudioInputStream(Sound));
             clip.start();
+            fc = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
 
             JumbleRumble jr = new JumbleRumble();
             jr.easyLevelOne();
 
-            Thread.sleep(clip.getMicrosecondLength()/1000);
+            //Thread.sleep(clip.getMicrosecondLength()/1000);
         } catch (Exception e) {
             System.out.println("no music found");
         }
